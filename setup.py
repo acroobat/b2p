8250ef948a109a35d41cb3fe3f3c4dc90c852ac9
acroobat <acroobat@mail.ru>
2021-06-06 18:44:03 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,25 @@
-imports setuptools
+#!/usr/bin/python3
+import setuptools
+
+import os.path
+import warnings
+import sys
+
+if setuptools_available:
+        params['scripts'] = ['bin/b2p']
+
+setup(
+    name='b2p',
+    version=__version__,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    url='https://github.com/acroobat/youtube-dl',
+    author='Roman Beslik',
+    author_email='rabeslik@gmail.com',
+    maintainer='acroobat',
+    maintainer_email='acroobat@mail.ru',
+    license='Unlicense',
+    packages=[
+        'b2p'],
+)
+
